4e19e7edd386d939ed06f34e4e0db429ed5ded23
Megatron19 <Megatron19@163.com>
2026-02-26 11:48:39 +0800

    style(format): 格式化代码以符合团队样式指南
    - 标准化 Buttons.vue 中 SVG 标签的缩进和格式
    - 组织 notificationService.ts 中的导入语句格式
    - 规范 timerStore.ts 中的导入语句格式
    - 统一文件中的代码风格和可读性

--- src/components/Buttons.vue
+++ src/components/Buttons.vue
@@ -46,9 +46,7 @@
             for="drawer"
             class="drawer-button btn btn-ghost btn-circle w-18 h-18 scale-95 tooltip tooltip-info"
             data-tip="设置">
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 32 32">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
                 <g fill="none">
                     <path
                         d="M10 18a2 2 0 1 0 0-4a2 2 0 0 0 0 4zm6 0a2 2 0 1 0 0-4a2 2 0 0 0 0 4zm8-2a2 2 0 1 1-4 0a2 2 0 0 1 4 0zm6 0c0 7.732-6.268 14-14 14S2 23.732 2 16S8.268 2 16 2s14 6.268 14 14zm-2 0c0-6.627-5.373-12-12-12S4 9.373 4 16s5.373 12 12 12s12-5.373 12-12z"
